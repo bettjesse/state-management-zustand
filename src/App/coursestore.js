8a9a08596b0ceee28930cf5579b3b40f06fa32ab
jesse <jessebett1999@gmail.com>
2022-11-17 12:34:00 +0300

    added comments on the zustand store

--- src/App/coursestore.js
+++ src/App/coursestore.js
@@ -3,19 +3,22 @@ import { devtools, persist } from "zustand/middleware"
 
 const coureStore = (set) => (
     {
+        //initial state
         courses: [],
+        //actions or manipulators to the data
         addCourse: (course) => {
             set(state => ({
                 courses: [course, ...state.courses]
             }))
 
         },
-
+// removeCourse action
         removeCourse:(courseId)=>{
             set(state=> ({
                courses: state.courses.filter((c)=> c.id !== courseId)
             }))
         },
+        //toggleCourse action
         toggleCourseStatus: (courseId)=> {
             set(state=>{
                 courses:state.courses.map(course=> course.id=== courseId ?{...course,completed: !course.completed} :course)
